c9b6e8aa2ff158e664bed9dfb992d7ee04221677
Gabrielius <gdobrovolskis97@gmail.com>
2022-06-07 23:24:01 +0100

    tried fetching convos

--- src/components/friends/Friends.jsx
+++ src/components/friends/Friends.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Friend from "./Friend";
 import "./friend.css";
 
 export default function Friends() {
+  const [convos, setConvos] = useState([]);
+  useEffect(() => {
+    console.log("friend effect trigger");
+    fetch("http://localhost:8080/convo", {
+      credentials: "include",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((data) =>
+      data.json().then((value) => {
+        setConvos(value);
+        console.log(value.conv);
+      })
+    );
+  }, [convos]);
   return (
     <div className="friendsWrapper">
+      {convos.map((value) => {
+        return <Friend>{value.username}</Friend>;
+      })}
       <Friend></Friend>
       <Friend></Friend>
       <Friend></Friend>
