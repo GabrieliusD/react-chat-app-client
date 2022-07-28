8673978568c792ec597caba41a59f77ec79f326a
Gabrielius <gdobrovolskis97@gmail.com>
2022-07-28 23:33:04 +0100

    https socket url

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -49,7 +49,7 @@ export default function Dashboard() {
     );
   };
   useEffect(() => {
-    socket.current = io("wss://gabkis.com/api", {
+    socket.current = io("https://gabkis.com/api", {
       withCredentials: true,
       path: "/api/socket.io/",
     });
